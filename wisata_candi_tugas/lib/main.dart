dadb1552899a8edff2237603433e4ca5ca2906d1
f <rifaldif888@gmail.com>
2024-10-26 09:30:30 +0700

    commit

--- wisata_candi_tugas/lib/main.dart
+++ wisata_candi_tugas/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi_faldi/screens/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+    home: ProfileScreen(),
+    //   home: DetailScreen(),
     );
 }
 }
